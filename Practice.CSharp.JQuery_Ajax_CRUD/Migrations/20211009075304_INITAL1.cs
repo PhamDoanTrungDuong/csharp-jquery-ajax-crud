1cff6ed685deb74dfdb384858064d3031544551f
Dương Phạm <pdtduong03@gmail.com>
2021-10-09 17:20:22 +0700

    Add project files.

---
+++ Practice.CSharp.JQuery_Ajax_CRUD/Migrations/20211009075304_INITAL1.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Practice.CSharp.JQuery_Ajax_CRUD.Migrations
+{
+    public partial class INITAL1 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "TrasactionId",
+                table: "Transactions",
+                newName: "TransactionId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "TransactionId",
+                table: "Transactions",
+                newName: "TrasactionId");
+        }
+    }
+}
---
+++ Practice.CSharp.JQuery_Ajax_CRUD/Models/TransactionModel.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Practice.CSharp.JQuery_Ajax_CRUD.Models
+{
+    public class TransactionModel
+    {
+        [Key]
+        public int TransactionId { get; set; }
+
+        [Column(TypeName = "nvarchar(12)")]
+        [DisplayName("Account Number")]
+        [Required(ErrorMessage = "this field is Require")]
+        public string AccountNumber { get; set; }
+
+        [Column(TypeName = "nvarchar(100)")]
+        [DisplayName("Beneficiary Name")]
+        [Required(ErrorMessage = "this field is Require")]
+        public string BeneficiaryName { get; set; }
+
+        [Column(TypeName = "nvarchar(100)")]
+        [DisplayName("Bank Name")]
+        [Required(ErrorMessage = "this field is Require")]
+        public string BankName { get; set; }
+
+        [Column(TypeName = "nvarchar(11)")]
+        [DisplayName("SWIFT Code")]
+        [Required(ErrorMessage = "this field is Require")]
+        public string SWIFTCode { get; set; }
+
+        [Required(ErrorMessage = "this field is Require")]
+        public int Amount { get; set; }
+
+        [DisplayFormat(DataFormatString = "{0:MM/dd/yyy}")]
+        public DateTime Date { get; set; }
+    }
+}
